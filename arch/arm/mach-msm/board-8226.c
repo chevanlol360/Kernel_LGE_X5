84912d66ec7ed4925a7c58bdb9d7062006492f48
Chevanlol360 <chevanlol360@CQ-ABA-HP.(none)>
2014-11-13 15:10:29 -0500

    Intelli-thermal Imported
    Imports Faux123 intelli thermal drivers
    
    Signed-off-by: Chevanlol360 <chevanlol360@CQ-ABA-HP.(none)>

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -126,7 +126,11 @@ void __init msm8226_add_drivers(void)
 	else
 		msm_clock_init(&msm8226_clock_init_data);
 	tsens_tm_init_driver();
-	msm_thermal_device_init();
+#ifdef CONFIG_INTELLI_THERMAL
+	msm_thermal_init(NULL);
+#else
+	msm_thermal_device_init(NULL);
+#endif
 }
 
 void __init msm8226_init(void)
